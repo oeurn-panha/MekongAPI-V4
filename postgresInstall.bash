c8dbd902392ea37d350ddb6872a0b049a95a6e90
OEURN Panha <96670448+oeurn-panha@users.noreply.github.com>
2024-09-22 14:50:31 +0700

    init

---
+++ postgresInstall.bash
@@ -0,0 +1,4 @@
+sudo apt install postgresql postgresql-contrib
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+psql --version
---
+++ remote.bash
@@ -0,0 +1,2 @@
+chmod 400 "mekongOhio.pem"
+ssh -i "mekongOhio.pem" ubuntu@ec2-3-22-188-141.us-east-2.compute.amazonaws.com
